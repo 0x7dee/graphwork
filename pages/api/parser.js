3ee1897a1a3a964910ada439e9f59efa3c18cb90
rxthew <richardtheumaster@gmail.com>
2022-09-02 23:59:27 +0200

    rough code re grouping  by attribute

--- pages/api/parser.js
+++ pages/api/parser.js
@@ -2,15 +2,30 @@
 const Graph = require('graphology')
 const gefx = require('graphology-gexf')
 
-const parser = function(gefxFile){
+let groupNumbers = {}
+
+const groupByAttribute = function(graph,node, attr){
+    let value = graph.getNodeAttribute(node,attr)
+    if(Object.keys(groupNumbers).includes(value)){
+        return groupNumbers[value]
+    }
+    groupNumbers[value] = Object.keys(groupNumbers).length + 1
+    return groupNumbers[value]
+
+}
+
+const parser = function(gefxFile, attr, groupingFunction=groupByAttribute){
+
     let graph = {
         nodes: [],
         links : []
     }
 
     let semiParsedGraph = gefx.parse(Graph,gefxFile)
+
     semiParsedGraph.forEachNode(node => graph.nodes.push({
-        "id" : node
+        "id" : node,
+        "group" : groupingFunction(graph,node,attr)
     }))
     semiParsedGraph.forEachEdge((ed,atts,source,target) => graph.links.push({
         "source": source,
